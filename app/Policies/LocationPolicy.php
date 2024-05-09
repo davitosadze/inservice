a8e64a4a54810d83a91959ad5a9f7cd0be859239
davitosadze <39856965+davitosadze@users.noreply.github.com>
2024-05-10 01:47:13 +0400

    fix

--- app/Policies/LocationPolicy.php
+++ app/Policies/LocationPolicy.php
@@ -33,6 +33,6 @@ class LocationPolicy
 
     public function delete(User $user, Location $location)
     {
-        return (($user->can("ლოკაციის წაშლა")) || $user->hasRole('director')) ? Response::allow() : Response::deny('არ გაქვთ ნებართვა!');
+        return ($user->can("ლოკაციის წაშლა")) ? Response::allow() : Response::deny('არ გაქვთ ნებართვა!');
     }
 }
