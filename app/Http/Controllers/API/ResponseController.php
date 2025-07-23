d7dccf0313a138816ed3d124459c3769cc2999eb
davitosadze <davitosadze7@gmail.com>
2025-07-23 21:48:26 +0400

    fix

--- app/Http/Controllers/API/ResponseController.php
+++ app/Http/Controllers/API/ResponseController.php
@@ -44,7 +44,9 @@ class ResponseController extends Controller
 
         if ($request->get("type") == "done") {
             $responses = $responses->where("status", 3)
-                // ->orWhere("status", 0)
+                ->orWhere("status", 0)
+                                ->take(10)
+
                 ->get();
         } elseif($request->get('type') == 'client-pending') {
             $responses = $responses->where("status", 4)
