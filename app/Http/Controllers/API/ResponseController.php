3433f69b862beb502164798d23a6e90a8c7b32d4
Giorgi Gordiashvili <giorgigordiashvili@mackbooks-MacBook-Pro.local>
2025-07-24 05:44:38 +0400

    ინჟინერი

--- app/Http/Controllers/API/ResponseController.php
+++ app/Http/Controllers/API/ResponseController.php
@@ -26,8 +26,14 @@ class ResponseController extends Controller
     $responsesQuery = Response::with(['user', 'purchaser', 'region', 'systemOne', 'systemTwo', 'performer'])
         ->orderBy('id', 'desc');
 
+         if (Auth::user()->roles->contains('name', 'ინჟინერი')) {
+            $responses = Response::with(['user', 'purchaser', 'region',  'systemOne', 'systemTwo', 'performer'])->orderBy('id', 'desc')
+                ->whereIn("status", [1, 5, 10])
+                ->where("performer_id", Auth::user()->id);
+        } elseif
+
     // Case 1: Show all done responses (status 0 or 3)
-    if ($type === 'done') {
+     ($type === 'done') {
             $responses = $responsesQuery->whereIn('status', [0, 3])->take(200)->get();
 
     // Case 2: Show all client-pending responses (status 4)
